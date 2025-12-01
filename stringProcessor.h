c918f1d861df0342005b4f53d173fdf41b1f23d0
xavvitee <leragoncharova1809@gmail.com>
2025-12-01 16:44:17 +0200

    додавання файлів

--- stringProcessor.h
+++ stringProcessor.h
@@ -1,119 +1,112 @@
 #ifndef STRING_PROCESSOR
 #define STRING_PROCESSOR
-
 #include <stdint.h>
 #include <stdbool.h>
-#include <stdlib.h>
+#include <malloc.h>
 #include <ctype.h>
 #include <stdio.h>
 #include "sort.h"
-
 #define STRING_MAX 512ull
-
-// Читання рядка
-int32_t get_str(const char* msg, char* str, int32_t limit)
+const int32_t get_str(const char* msg, char* str, const int32_t limit)
 {
     printf("%s", msg);
     char c;
     size_t size = 0;
-
     while (size < (size_t)(limit - 1) && (c = getchar()) != EOF && c != '\n')
     {
         str[size++] = c;
     }
-
     str[size] = '\0';
     return (int32_t)size;
 }
-
-// Аналог strlen
-size_t strlenn(const char* str)
+const size_t strlenn(const char* str)
 {
     size_t cnt = 0;
-    while (str[cnt] != '\0')
+    while (*str++)
+    {
         cnt++;
+    }
     return cnt;
 }
-
-// Копіювання рядка
-void strcopy(char* dest, const char* src, size_t until)
+void strcopy(char* fStr, char* sStr, size_t until)
 {
-    while (until-- > 0 && (*dest++ = *src++));
+    while (until-- > 0 && (*sStr++ = *fStr++))
+        ;
 }
-
-// Порівняння рядків
-int32_t strcmpp(const char* a, const char* b)
+int32_t strcmpp(const char* fStr, const char* sStr)
 {
-    while (*a && *a == *b)
+    while (*fStr && *fStr == *sStr)
     {
-        a++;
-        b++;
+        fStr++;
+        sStr++;
     }
-    return (int32_t)((unsigned char)*a - (unsigned char)*b);
+    return (int32_t)((unsigned char)*fStr - (unsigned char)*sStr);
 }
-
-// Конкатенація
 char* strcatt(char* fStr, const char* sStr)
 {
     char* ptr = fStr + strlenn(fStr);
-    while (*sStr)
+    while (*sStr != '\0')
     {
         *ptr++ = *sStr++;
     }
     *ptr = '\0';
     return fStr;
 }
-
-// Перевірка валідності
 bool is_string_valid(char* str)
 {
-    bool hasString = false;
-    bool hasColon = false;
+    bool isThereString = false;
+    bool isThereColon = false;
     const size_t size = strlenn(str);
-
     for (size_t i = 0; i < size; i++)
     {
-        if ((str[i] == ':' || str[i] == ';' || str[i] == ',') && i == 0)
+        if ((str[i] == ':' || str[i] == ';' || str[i] == ',') && i == 0ull)
+        {
             return false;
-
+        }
         if (str[i] == ':')
         {
-            if (hasColon) return false;
-            hasColon = true;
-            continue;
+            isThereColon = true;
         }
-
-        if (hasColon)
+        else if (isThereColon)
         {
             if (str[i] == ',')
             {
-                if (!isalpha((unsigned char)str[i - 1]) && str[i - 1] != ' ')
+                if (!isalpha(str[i - 1ull]) && str[i - 1ull] != ' ')
+                {
                     return false;
-                hasString = true;
+                }
+                else
+                {
+                    isThereString = true;
+                }
             }
-
             if (str[i] == ';')
             {
-                if (!hasString || (!isalpha((unsigned char)str[i - 1]) && str[i - 1] != ' '))
+                if (!isThereString || (!isalpha(str[i - 1ull]) && str[i - 1ull] != ' '))
+                {
                     return false;
-
-                hasColon = false;
+                }
+                else
+                {
+                    isThereColon = false;
+                }
+            }
+            if (str[i] == ':')
+            {
+                return false;
             }
         }
+        else
+        {
+            isThereString = false;
+        }
     }
-
     return true;
 }
-
-// Обробка рядка
 char* process_string(char* str)
 {
     const size_t length = strlenn(str);
-
-    // +1 для '\0'
-    char* result = (char*)calloc(length + 1, sizeof(char));
-    result[0] = '\0';  // Ініціалізуємо пустий рядок
-
+    char* result = (char*)calloc(length, sizeof(char));
     for (size_t i = 0; i < length; i++)
     {
         if (str[i] != ':')
@@ -122,60 +115,54 @@ char* process_string(char* str)
             strcatt(result, temp);
             continue;
         }
-
-        if (i + 1 >= length || str[i + 1] != ' ')
+        if (str[i + 1] != ' ')
+        {
             return NULL;
-
+        }
         strcatt(result, ": ");
-
-        const size_t maxWords = 100;
-        char** arr = (char**)calloc(maxWords, sizeof(char*));
-
-        for (size_t w = 0; w < maxWords; w++)
-            arr[w] = (char*)calloc(STRING_MAX, sizeof(char));
-
-        size_t wordCount = 0;
-
-        // Зчитування слів
-        for (size_t j = i + 2; j < length; j++)
+        const size_t strsPreCnt = 100;
+        char** arr = (char**)calloc(strsPreCnt, sizeof(char*));
+        for (size_t strsCnt = 0; strsCnt < strsPreCnt; strsCnt++)
+        {
+            arr[strsCnt] = (char*)calloc(STRING_MAX, sizeof(char));
+        }
+        for (size_t j = i + 2ull, strRealCnt = 0ull; j < length; j++)
         {
             if (str[j] == ',')
             {
-                wordCount++;
-                j++;
+                ++strRealCnt;
+                ++j;
             }
             else if (str[j] == ';')
             {
-                // Сортування
-                quick_sort(arr, wordCount + 1);
-
-                // Додавання у результат
-                for (size_t k = 0; k <= wordCount; k++)
+                quick_sort(arr, ++strRealCnt);
+                for (size_t k = 0; k < strRealCnt; k++)
                 {
                     strcatt(result, arr[k]);
-                    if (k == wordCount)
+                    if (k == strRealCnt - 1ull)
+                    {
                         strcatt(result, ";");
+                    }
                     else
+                    {
                         strcatt(result, ", ");
+                    }
                 }
-
                 i = j;
                 break;
             }
             else
             {
-                char t[2] = { str[j], '\0' };
-                strcatt(arr[wordCount], t);
+                char temp[2] = { str[j], '\0' };
+                strcatt(arr[strRealCnt], temp);
             }
         }
-
-        // Очищення
-        for (size_t w = 0; w < maxWords; w++)
-            free(arr[w]);
+        for (size_t strsCnt = 0; strsCnt < strsPreCnt; strsCnt++)
+        {
+            free(arr[strsCnt]);
+        }
         free(arr);
     }
-
     return result;
 }
-
 #endif
